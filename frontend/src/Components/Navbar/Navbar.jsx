e4ebed0674ce07e8bc74765ae995e51157cd2404
muzayenadem <muzynadem@gmail.com>
2024-08-24 12:54:44 +0300

    desing change on top page

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,63 +1,80 @@
 import { MdDarkMode } from "react-icons/md";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { img1 } from "../Images/images";
+import {
+  Carousel,
+  initTWE,
+} from "tw-elements";
 
+initTWE({ Carousel });
 function Navbar() {
+  const [isOpen,setIsOpen] = useState(false)
+  const data = [img1,img1,img1]
+  const [me,setMe] = useState('data-twe-carousel-active')
+
+  useEffect(()=>{
+    setMe('data-twe-carousel-active')
+  },[])
   return (
     <>
-    <section class="bg-white dark:bg-gray-900">
-    <nav x-data="{ isOpen: false }" class="container p-6 mx-auto lg:flex lg:justify-between lg:items-center">
+    <section className="bg-white dark:bg-gray-900">
+    <nav x-data={{ isOpen: false }} className="container p-6 mx-auto lg:flex lg:justify-between lg:items-center">
         <div class="flex items-center justify-between">
             <a href="#">
                 <img className="w-auto h-6 sm:h-7" src="https://merakiui.com/images/full-logo.svg" alt=""/>
             </a>
 
         
-            <div class="flex lg:hidden">
-                <button x-cloak type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
-                    <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                        <path stroke-linecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
+            <div className="flex lg:hidden">
+                <button  type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                  {!isOpen && 
+                        <svg onClick={()=> setIsOpen(true)}  xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
                     </svg>
-            
-                    <svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                        <path stroke-linecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
+                  }
+                  {isOpen &&
+                   <svg onClick={()=> setIsOpen(false)}  xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                   <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+               </svg>
+                  }
                 </button>
             </div>
         </div>
 
        
-        <div x-cloak :class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']" class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white shadow-md lg:bg-transparent lg:dark:bg-transparent lg:shadow-none dark:bg-gray-900 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center">
+        <div  className={`${isOpen ? 'translate-x-0 opacity-100 ':'opacity-0 -translate-x-full'} absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white shadow-md lg:bg-transparent lg:dark:bg-transparent lg:shadow-none dark:bg-gray-900 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center`}>
             <div className="flex flex-col space-y-4 lg:mt-0 lg:flex-row lg:-px-8 lg:space-y-0">
-                <a className="text-gray-700 transition-colors duration-300 transform lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#">Home</a>
-                <a className="text-gray-700 transition-colors duration-300 transform lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#">Components</a>
-                <a className="text-gray-700 transition-colors duration-300 transform lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#">Pricing</a>
+                <a className="text-gray-700 transition-colors duration-300 transform lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#">History</a>
+                <a className="text-gray-700 transition-colors duration-300 transform lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#">Healthy</a>
+                <a className="text-gray-700 transition-colors duration-300 transform lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#">Industry</a>
+                <a className="text-gray-700 transition-colors duration-300 transform lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#">Contructions</a>
+                <a className="text-gray-700 transition-colors duration-300 transform lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#">Edjucation</a>
                 <a className="text-gray-700 transition-colors duration-300 transform lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#">Contact</a>
             </div>
 
-            <a class="block px-5 py-2 mt-4 text-sm text-center text-white capitalize bg-blue-600 rounded-lg lg:mt-0 hover:bg-blue-500 lg:w-auto" href="#">
-                Get started
+            <a className="block px-5 py-2 mt-4 text-sm text-center text-white capitalize bg-blue-600 rounded-lg lg:mt-0 hover:bg-blue-500 lg:w-auto" href="#">
+                See News
             </a>
             <span onClick={()=>alert ('')} className="text-neutral-200 data-theme-light text-2xl font-bold ml-5"><MdDarkMode/></span>
         </div>
     </nav>
-    
+
 
     <div
   id="carouselExampleCaptions"
-  class="relative"
+  className="relative h-[70vh]"
   data-twe-carousel-init
   data-twe-ride="carousel">
-
+  {/* <!--Carousel indicators--> */}
   <div
-    class="absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
+    className="absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
     data-twe-carousel-indicators>
     <button
       type="button"
       data-twe-target="#carouselExampleCaptions"
       data-twe-slide-to="0"
       data-twe-carousel-active
-      class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+      className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
       aria-current="true"
       aria-label="Slide 1"></button>
     <button
@@ -70,89 +87,95 @@ function Navbar() {
       type="button"
       data-twe-target="#carouselExampleCaptions"
       data-twe-slide-to="2"
-      class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+      className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
       aria-label="Slide 3"></button>
   </div>
 
+  {/* <!--Carousel items--> */}
   <div
-    class="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+    class="relative w-full h-[70vh] capitalize overflow-hidden after:clear-both after:block after:content-[0]">
+    {/* <!--First item--> */}
     <div
-      class="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+      class="relative float-left -mr-[100%] w-full h-full transition-transform duration-[400ms] ease-in-out motion-reduce:transition-none"
+      data-twe-carousel-item
       data-twe-carousel-active
+      style={{backfaceVisibility:'hidden'}}>
+      <img
+        src={img1}
+        className="block w-full h-full opacity-60 "
+        alt="..." />
+      <div
+        className="absolute inset-x-[25%] bottom-24 hidden py-5 text-center text-white md:block">
+        <h5 className="text-3xl font font-bold capitalize">Dodola Industrail park</h5>
+        <p className="text-xl font-bold">
+          Some representative placeholder content for the first slide.
+        </p>
+      </div>
+    </div>
+    {/* <!--Second item--> */}
+    <div
+      className="relative float-left -mr-[100%] hidden w-full  h-full  transition-transform duration-[400ms] ease-in-out motion-reduce:transition-none"
       data-twe-carousel-item
       style={{backfaceVisibility:'hidden'}}>
       <img
         src={img1}
-        class="block w-full opacity-60"
+        class="block w-full h-full opacity-60"
         alt="..." />
-      <div
-        class="absolute inset-x-[15%] bottom-[20%] hidden py-5 text-center text-white md:block">
-        <h5 class="text-3xl  font-bold">Dodola Official website</h5>
-        <p className="text-2xl font-bold ">
-         The fast growing smart city in ethiopia oromia region
+        <div
+        className="absolute inset-x-[25%] bottom-24 hidden py-5 text-center text-white md:block">
+        <h5 className="text-3xl font font-bold">Dodola Industrail park</h5>
+        <p className="text-xl font-bold">
+          Some representative placeholder content for the first slide.
         </p>
       </div>
     </div>
+    {/* <!--Third item--> */}
     <div
-      class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+      className="relative float-left -mr-[100%] hidden w-full  h-full  transition-transform duration-[400ms] ease-in-out motion-reduce:transition-none"
       data-twe-carousel-item
       style={{backfaceVisibility:'hidden'}}>
       <img
-        src="https://tecdn.b-cdn.net/img/Photos/Slides/img%20(22).jpg"
-        class="block w-full"
+        src={img1}
+        class="block w-full h-full opacity-60"
         alt="..." />
       <div
-        class="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
-        <h5 class="text-xl">Second slide label</h5>
-        <p>
-          Some representative placeholder content for the second slide.
-        </p>
-      </div>
-    </div>
-    <div
-      class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-      data-twe-carousel-item
-      style={{backfaceVisibility:'hidden'}}>
-      <img
-        src="https://tecdn.b-cdn.net/img/Photos/Slides/img%20(23).jpg"
-        class="block w-full"
-        alt="..." />
-      <div
-        class="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
-        <h5 class="text-xl">Third slide label</h5>
-        <p>
-          Some representative placeholder content for the third slide.
+        className="absolute inset-x-[25%] bottom-24 hidden py-5 text-center text-white md:block">
+        <h5 className="text-3xl font font-bold">Dodola Industrail park</h5>
+        <p className="text-xl font-bold">
+          Some representative placeholder content for the first slide.
         </p>
       </div>
     </div>
   </div>
-  
+
+  {/* <!--Carousel controls - prev item--> */}
   <button
-    class="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+    className="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
     type="button"
     data-twe-target="#carouselExampleCaptions"
     data-twe-slide="prev">
-    <span class="inline-block h-8 w-8">
+    <span className="inline-block h-8 w-8">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke="currentColor"
-        class="h-6 w-6">
+        className="h-6 w-6">
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M15.75 19.5L8.25 12l7.5-7.5" />
       </svg>
     </span>
     <span
-      class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
       >Previous</span
     >
   </button>
+  {/* <!--Carousel controls - next item--> */}
   <button
-    class="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+    className="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
     type="button"
     data-twe-target="#carouselExampleCaptions"
     data-twe-slide="next">
@@ -161,35 +184,35 @@ function Navbar() {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke="currentColor"
-        class="h-6 w-6">
+        className="h-6 w-6">
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M8.25 4.5l7.5 7.5-7.5 7.5" />
       </svg>
     </span>
     <span
-      class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
       >Next</span
     >
   </button>
 </div>
 
-    <div class="container px-6 py-16 mx-auto text-center">
-        <div class="max-w-lg mx-auto">
-            <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Welcome to dodola city with a full of intention</h1>
-            <p class="mt-6 text-gray-500 dark:text-gray-300">Dodola City is a picturesque town in Ethiopia's Oromia Region, known for its rich cultural heritage and stunning natural beauty. Nestled in the highlands near the Bale Mountains, Dodola offers breathtaking landscapes and outdoor activities. 
+    <div className="container px-6 py-16 mx-auto text-center">
+        <div className="max-w-lg mx-auto">
+            <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Welcome to dodola city with a full of intention</h1>
+            <p className="mt-6 text-gray-500 dark:text-gray-300">Dodola City is a picturesque town in Ethiopia's Oromia Region, known for its rich cultural heritage and stunning natural beauty. Nestled in the highlands near the Bale Mountains, Dodola offers breathtaking landscapes and outdoor activities. 
                 obcaecati illum mollitia.</p>
             <button class="px-5 py-2 mt-6 text-sm font-medium leading-5 text-center text-white capitalize bg-blue-600 rounded-lg hover:bg-blue-500 lg:mx-0 lg:w-auto focus:outline-none">
-                Start enjoying with dodola's fresh air
+                Start enjoying with dodola's fresh
             </button>
-            <p class="mt-3 text-sm text-gray-400 ">feel free to contact us </p>
+            <p className="mt-3 text-sm text-gray-400 ">feel free to contact us </p>
         </div>
 
-        <div class="flex justify-center mt-10">
-            <img class="object-cover w-full h-96 rounded-xl lg:w-4/5" src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" />
+        <div className="flex justify-center mt-10">
+            <img className="object-cover w-full h-96 rounded-xl lg:w-4/5" src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" />
         </div>
     </div>
 </section>
