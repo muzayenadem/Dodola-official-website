06fbca27d5eddd6bdf6cdf5483a161deee3d29c6
muzayenadem <muzynadem@gmail.com>
2024-09-10 00:55:36 +0300

    content post feuture

--- frontend/src/Components/AdminComponents/PostComponents/MainPost/SubmitionPage.jsx
+++ frontend/src/Components/AdminComponents/PostComponents/MainPost/SubmitionPage.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-
+import axios from 'axios'
+import React, { useState } from 'react'
+import {useSelector} from 'react-redux'
+import { serverLink } from '../../../../Controller/CommonLinks/ServerLink'
 function SubmitionPage({setMethod,method}) {
-    const clickHandler = () =>{
-        setMethod({...method,submitionPage:false,congraPage:true})
+  const [err,setErr] = useState('')
+    const clickHandler = async () =>{
+      try {
+            const formData = new FormData()
+            formData.append('data',JSON.stringify(data))
+            for(let i = 0; i <= images.length; i++){
+              formData.append('files',images[i]) 
+            }
+          
+          const response = await axios.post(`${serverLink}/post-main-content`, formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+            withCredentials: true, // This is important for sending credentials
+          })
+          .then( (res) => {
+            return res
+          })
+          .catch( (err) =>{
+            return err
+          })
+          console.log({response})
+
+          if(response.data){
+            setMethod({...method,submitionPage:false,congraPage:true})
+          }
+          if(response.response.data){
+            setErr(response.response.data)
+          }
+      } catch (error) {
+        console.log({error:error.message})
+      }
     }
+    const data = useSelector(state => state.mainPost.data)
+    const images = useSelector(state => state.mainPost.images)
+  
   return (
     <div class="container px-6 py-6 mx-auto text-center">
         <div class="max-w-lg mx-auto">
+          <h1>{err}</h1>
             <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Almost Done you just have to click the button bellow</h1>
             <p class="mt-6 text-gray-500 dark:text-gray-300">please be sure first that everything is as you want before publish</p>
             <button onClick={clickHandler}  class="px-5 py-2 mt-6 text-sm font-medium leading-5 text-center text-white capitalize bg-blue-600 rounded-lg hover:bg-blue-500 lg:mx-0 lg:w-auto focus:outline-none">
