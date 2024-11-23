0800d7369d3468f6c548d504a83804cbbab33bd6
muzayenadem <muzynadem@gmail.com>
2024-11-23 09:41:27 +0300

    gallery paged fixed

--- frontend/src/Components/AppComponents/About/History/TopHistory.jsx
+++ frontend/src/Components/AppComponents/About/History/TopHistory.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { galma1, galma2, logo1 } from '../../Images/images'
 
 function TopHistory() {
   return (
@@ -23,26 +24,27 @@ function TopHistory() {
        
         <section class="relative bg-gradient-to-br from-blue-900 to-indigo-800 text-white overflow-hidden">
             <div class="absolute inset-0 bg-black opacity-50"></div>
-            <div class="absolute inset-0 bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80')]"></div>
+            <div class="absolute inset-0 bg-cover bg-center ] opacity-55" style={{backgroundImage:`url(${galma1})`}}></div>
             
             <div class="container mx-auto px-4 py-24 md:py-32 relative z-10">
                 <div class="flex flex-col md:flex-row items-center justify-between">
                   
                     <div class="w-full md:w-1/2 mb-12 md:mb-0">
                         <h1 class="text-5xl md:text-6xl font-bold mb-6 leading-tight">
-                            History of<br/>Dodola City.<br/>
+                            History of<br/>Dodola City.<br/>Adminstration
                         </h1>
                         <p class="text-xl mb-8 text-gray-300">Feel free to know more about dodola city with reading its history</p>
-                        <div class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
+                        {/* <div class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
                             <a href="#" class="bg-white text-blue-900 font-semibold px-8 py-3 rounded-full hover:bg-blue-100 transition duration-300 text-center">Get Started</a>
                             <a href="#" class="border-2 border-white text-white font-semibold px-8 py-3 rounded-full hover:bg-white hover:text-blue-900 transition duration-300 text-center">Learn More</a>
-                        </div>
+                        </div> */}
                     </div>
                     
     
-                    <div class="w-full md:w-1/2 md:pl-12">
+                    <img src={logo1} className='w-auto rounded-lg h-96 '></img>
+                           {/* <div class="w-full md:w-1/2 md:pl-12">
                         <div class="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-xl p-8 shadow-2xl">
-                            <h2 class="text-2xl font-semibold mb-6">Why MyCompany?</h2>
+                            <h2 class="text-2xl font-semibold mb-6">Dodola City History?</h2>
                             <ul class="space-y-4">
                                 <li class="flex items-center">
                                     <svg class="w-6 h-6 mr-3 text-yellow-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
@@ -58,7 +60,7 @@ function TopHistory() {
                                 </li>
                             </ul>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         
--- frontend/src/Components/AppComponents/Home/VMG.jsx
+++ frontend/src/Components/AppComponents/Home/VMG.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { logo1 } from '../Images/images'
+import { logo1, night1, night2 } from '../Images/images'
 import { Link, useNavigate } from 'react-router-dom';
 
 function VMG() {
@@ -24,7 +24,7 @@ function VMG() {
     <main class="">
         <section class="relative bg-gradient-to-br from-blue-900 to-indigo-800 text-white overflow-hidden">
             <div class="absolute inset-0 bg-black opacity-50"></div>
-            <div class="absolute inset-0 bg-cover bg-center "  ></div>
+            <div class="absolute inset-0 bg-cover bg-center " style={{backgroundImage:`url(${night2})`}}  ></div>
             
             <div class="container mx-auto px-4 py-24 md:py-32 relative z-10">
                 <div class="flex flex-col md:flex-row items-center justify-between">
