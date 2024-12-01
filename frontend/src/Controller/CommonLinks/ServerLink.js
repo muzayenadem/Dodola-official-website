8d34f158daaa563f9d181946a746d07c8af675de
muzayenadem <muzynadem@gmail.com>
2024-12-01 11:23:47 +0300

    some change

--- frontend/src/Controller/CommonLinks/ServerLink.js
+++ frontend/src/Controller/CommonLinks/ServerLink.js
@@ -1,6 +1,6 @@
-export const serverLink = 'http://localhost:3600'
+export const serverLink = 'https://dodola-official-website.onrender.com'
+//'http://localhost:3600'
 //'https://dodola-official-website.onrender.com'
- //'http://localhost:3600'
 
 
 export const rentalHouseLink = 'https://gofind-om6x.onrender.com'
