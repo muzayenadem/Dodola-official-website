7f52e168039d02381b99b048bb44a7c385e7a972
muzayenadem <muzynadem@gmail.com>
2025-01-05 07:36:38 +0300

    something at frontend

--- frontend/src/Components/AppComponents/News/Events/LatestNews.jsx
+++ frontend/src/Components/AppComponents/News/Events/LatestNews.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { water2 } from '../../Images/images'
+import { Link } from 'react-router-dom'
 
 function LatestNews({latest}) {
     console.log({latest})
@@ -15,15 +16,19 @@ function LatestNews({latest}) {
             {latest.length &&
             latest.map((data,i)=>{
                 return (
-                    <div key={i} className="flex px-1 py-4">
-                    <img alt="" className="flex-shrink-0 object-cover w-20 h-20 mr-4 dark:bg-gray-500" src={data.files[0]} />
-                    <div className="flex flex-col flex-grow">
-                        <a rel="noopener noreferrer" href="#" className="font-serif hover:underline">{data.title}</a>
-                        <p className="mt-auto text-xs dark:text-gray-600">5 minutes ago
-                            <a rel="noopener noreferrer" href="#" className="block text-blue-600 lg:ml-2 lg:inline hover:underline">{data.category}</a>
-                        </p>
+                     <div key={i} className="">
+                        <Link to={`/news-detail/${data._id}`}>
+                        <div  className="flex px-1 py-4">
+                            <img alt="" className="flex-shrink-0 object-cover w-20 h-20 mr-4 dark:bg-gray-500" src={data.files[0]} />
+                        <div className="flex flex-col flex-grow">
+                            <a rel="noopener noreferrer" href="#" className="font-serif hover:underline">{data.title}</a>
+                            <p className="mt-auto text-xs dark:text-gray-600">5 minutes ago
+                                <a rel="noopener noreferrer" href="#" className="block text-blue-600 lg:ml-2 lg:inline hover:underline">{data.category}</a>
+                            </p>
+                        </div>
+                        </div>
+                        </Link>
                     </div>
-                </div>
                 )
             })}
 
