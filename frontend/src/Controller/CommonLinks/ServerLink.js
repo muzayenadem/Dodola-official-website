c785647e72d434845e75e28d1d1a897d174e3249
muzayenadem <muzynadem@gmail.com>
2025-01-05 05:34:45 +0300

    something at frontend

--- frontend/src/Controller/CommonLinks/ServerLink.js
+++ frontend/src/Controller/CommonLinks/ServerLink.js
@@ -1,5 +1,4 @@
-export const serverLink = 'http://localhost:3600'
- //'https://dodola-official-website.onrender.com' 
+export const serverLink = 'https://dodola-official-website.onrender.com' 
 
 
 export const rentalHouseLink = 'https://gofind-om6x.onrender.com'
