25308bbe93895962d36037f701eb50e6aa4e6e37
Muzayen Adem <muzynadem@gmail.com>
2025-02-18 06:48:36 -0800

    chacges on mobile design

--- frontend/src/Components/AdminComponents/ManageAdmins/AdminPlace/AdminTopButton.jsx
+++ frontend/src/Components/AdminComponents/ManageAdmins/AdminPlace/AdminTopButton.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Select from 'react-select';
+import { useDispatch, useSelector } from 'react-redux'
+// server side renders reducers
+import { Link, useNavigate } from 'react-router-dom'
+import { filterJobsFromServer, reFetchJobs } from '../../../../Controller/Data/jobsSlce';
 function AdminTopButton() {
+    const [isClearable, setIsClearable] = useState(true);
+    const [isSearchable, setIsSearchable] = useState(true);
+    const [isDisabled, setIsDisabled] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isRtl, setIsRtl] = useState(false);
+
+
+    const navigate = useNavigate('')
+    const dispatch = useDispatch()
+    const changeHandler = value =>{
+        dispatch(filterJobsFromServer(value.label))
+     }
+    const allHandler = () =>{
+      dispatch(reFetchJobs())
+    }
+
+    const contents = useSelector((state => state.content))
+    if(contents.loading == true){
+      return <div>Loading............</div>
+    }
+
+      const data = [
+        {value:1,label:'Goverbment & public Adminstration'},
+        {value:2,label:'Healthycare'},
+        {value:3,label:'Education'},
+        {value:4,label:'Construction and infrustructure'},
+        {value:5,label:'Information Technology'},
+        {value:6,label:'Agriculture',},
+        {value:7,label:'Bussiness and Finance'},
+        {value:8,label:'Environmental and Natural Resources',},
+        {value:9,label:'Social Services'},
+        {value:10,label:'Media and Communication'}
+    ]
+      
   return (
-    <div>AdminTopButton</div>
+    <div className="flex justify-between">
+    <div className="flex gap-3">
+        <div className="relative" >
+
+
+        <button
+        onClick={allHandler}
+        className="flex items-center rounded bg-neutral-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-600 shadow-light-3 transition duration-150 ease-in-out hover:bg-neutral-200 hover:shadow-light-2 focus:bg-neutral-200 focus:shadow-light-2 focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-light-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+        type="button">All</button>
+       </div>
+
+
+        {/* <Select
+            className={` basic-single dark:text-black dark:bg-gray-900  lg:w-96  justify-center items-center`}
+            classNamePrefix="select"
+            defaultValue={{value:0,label:'Select Category'}}
+            isDisabled={isDisabled}
+            isLoading={true}
+            onChange={changeHandler}
+            isClearable={isClearable}
+            isRtl={isRtl}
+            isSearchable={isSearchable}
+            name="color"
+            options={data}
+        /> */}
+
+    </div>
+    <button
+        onClick={()=> navigate('/admin/add-admin')}
+    className="flex items-center text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg sm:w-auto gap-x-2 hover:bg-blue-700 dark:hover:bg-blue-500 dark:bg-blue-600 px-6 pb-2 pt-2.5 t font-medium uppercase leading-normal  shadow-light-3 ease-in-out hover:shadow-light-2 focus:bg-neutral-200 focus:shadow-light-2 focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-light-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+    type="button">
+         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clip-path="url(#clip0_3098_154395)">
+                    <path d="M13.3333 13.3332L9.99997 9.9999M9.99997 9.9999L6.66663 13.3332M9.99997 9.9999V17.4999M16.9916 15.3249C17.8044 14.8818 18.4465 14.1806 18.8165 13.3321C19.1866 12.4835 19.2635 11.5359 19.0351 10.6388C18.8068 9.7417 18.2862 8.94616 17.5555 8.37778C16.8248 7.80939 15.9257 7.50052 15 7.4999H13.95C13.6977 6.52427 13.2276 5.61852 12.5749 4.85073C11.9222 4.08295 11.104 3.47311 10.1817 3.06708C9.25943 2.66104 8.25709 2.46937 7.25006 2.50647C6.24304 2.54358 5.25752 2.80849 4.36761 3.28129C3.47771 3.7541 2.70656 4.42249 2.11215 5.23622C1.51774 6.04996 1.11554 6.98785 0.935783 7.9794C0.756025 8.97095 0.803388 9.99035 1.07431 10.961C1.34523 11.9316 1.83267 12.8281 2.49997 13.5832" stroke="currentColor" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"/>
+                    </g>
+                    <defs>
+                    <clipPath id="clip0_3098_154395">
+                    <rect width="20" height="20" fill="white"/>
+                    </clipPath>
+                    </defs>
+                </svg>
+                <span className='px-2'>Add Admin</span>
+
+  </button>
+  </div>
   )
 }
+
 
 export default AdminTopButton
--- frontend/src/Components/AdminComponents/ManageAdmins/AdminPlace/ListOfAdmins.jsx
+++ frontend/src/Components/AdminComponents/ManageAdmins/AdminPlace/ListOfAdmins.jsx
@@ -1,8 +1,161 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CiMenuKebab } from "react-icons/ci";
+import Clicks from './Clicks'
+import ContentPages from '../../ManageContent/ContentPlace/ContentPages';
+import DeletePopUp from '../../PopUps/DeletePopUp';
+import UnPuplishPopUp from '../../PopUps/UnPuplishPopUp';
+import axios from 'axios';
+import { serverLink } from '../../../../Controller/CommonLinks/ServerLink';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { reFetchEmployee } from '../../../../Controller/Data/employeeSlice';
+function ListOfAdmins({employee,data}) {
+    const [openDelete,setOpenDelete] = useState(false)
+    const [openPublish,setOpenPublish] = useState(false)
+    const [openId,setOpenId] = useState('') 
+    const [isOpen, setIsOpen] = useState(false)
+    const navigate = useNavigate('')
+    const openHandler = (id) =>{
+        if(id  & id != openId){
+            setIsOpen(false)
+            setIsOpen(true)
+            setIsOpen(id)
+        } else{
+            setOpenId(id)
+            setIsOpen(!isOpen)
+        }
+       }
 
-function ListOfAdmins() {
+       const dispatch = useDispatch()
+       const deleteHandler = async () =>{
+        try {
+            const response = await axios.post(`${serverLink}/delete-emPloyee`,{id:openId})
+            if(response.data){
+                setOpenDelete(false)
+                //dispatch(reFetchEmployee)
+                window.location.href = '/admin/employees'
+            }
+        } catch (error) {
+            if(error.response.data){
+                console.log('error respone data : ', error.response.data)
+                return error.response.data
+            }
+            if(error.request){
+                console.log('request error : ',error.request)
+                return error.request
+            }
+            if(error.message){
+                console.log('error message : ', error.message)
+                return error.message
+            }
+        } 
+      }
+
+
+      const unPublishHandler = () =>{
+        alert('Un Publish handler')
+      }
+      if(!data.data.length & data.loading == false){
+        return <div className='text-center py-32 min-h-[70vh] dark:bg-gray-900'>there is no data </div>
+      }
+    
   return (
-    <div>ListOfAdmins</div>
+    <>
+    <div className='bg-white dark:bg-gray-900 dark:text-white/80'>
+        <div className="flex justify-between p-3 gap-6 border-b-[2px] dark:border-b-neutral-700">
+            <div className="flex w-[8%]">Admins</div>
+            <div className="hidden md:flex w-[20%]">Role</div>
+            <div className="hidden md:flex w-[30%]">Name</div>
+            <div className="hidden md:flex w-[40%]">Contacts</div>
+            <button className="flex focus:outline-none w-[10%] font-body text-xl text-blue-700 justify-end items-center px-6">Edit</button>
+        </div>
+        {employee.map(({fname,lname,email,phone,profileImg,role,_id},i) =>{
+             const {eventManager,jobsManager,contentManager,responseManager,biddingManager,generalManager} = role
+            return (
+                <div key={i} className='bg-white border-b-[1px] h-24 border-b-neutral-300 dark:border-b-neutral-700 dark:bg-gray-900 p-3 dark:text-white/80'>
+                    <div className="flex justify-between gap-3 w-full">
+                        <div className="flex w-[30%] md:w-[8%]">
+                         <img src={profileImg} className='w-16 h-16 rounded-lg '/>
+                        </div>
+                        <div className=" w-[40%] hidden  md:flex-row md:w-[20%] md:flex   items-center">
+                            <p className='text-blue-800'>{fname} {lname}</p>
+                        </div>
+                        <div className="hidden md:flex w-[30%] items-center ">
+                            {eventManager  && !generalManager && <p>Event Manager</p>}
+                            {jobsManager && !generalManager &&<p>Jobs Manager</p>}
+                            {contentManager && !generalManager &&<p>Content Manager</p>}
+                            {biddingManager && !generalManager &&<p>Bidding Manager</p>}
+                            {responseManager &&!generalManager && <p>Response Manager</p>}
+                            {generalManager &&<p>General Manager</p>}
+                        </div>
+                        <div className="hidden md:flex w-[40%]  items-center">
+                            <div className="-gap-5">
+                                <p>{email}</p>
+                                <p>0{phone}</p>
+                            </div>
+                        </div>
+                        <div className=" hidden md:flex w-[10%] justify-end items-center px-6 relative">
+                            <button onClick={()=> openHandler(_id)} className=' text-xl w-10 h-10 rounded-lg text-center flex justify-center hover:bg-slate-200 dark:hover:bg-gray-700 items-center shadow-md dark:shadow-neutral-400 focus:outline-none'><CiMenuKebab/></button>
+                            <div onMouseLeave={()=> setIsOpen(false)} className={` shadow-lg shadow-neutral-500 rounded-md absolute z-10 -bottom-7 right-12 py-3 dark:bg-gray-900   w-48  bg-white ${isOpen == true & _id == openId  ? '':'hidden'} `}>
+                                 <Clicks 
+                                 isOpen={isOpen} 
+                                 setIsOpen={setIsOpen} 
+                                 setOpenDelete={setOpenDelete}
+                                 setOpenPublish={setOpenPublish}
+                                 employeeId={_id}
+                                 />
+                            </div>
+                        </div>
+                        <div className="flex md:hidden flex-col  w-[90%]">
+                            <div className="flex w-full gap-3">
+                                <div className="flex w-[90%] flex-col items-center">
+                                    <p className='text-blue-800'>{fname} {lname}</p>
+                                </div>
+                                <div className=" flex w-[5%] justify-end items-top relative md:px-6">
+                                     <button onClick={()=> openHandler(_id)} className=' text-xl  text-center flex justify-center items-center focus:outline-none'><CiMenuKebab/></button>
+                                     <div onMouseLeave={()=> setIsOpen(false)} className={` shadow-lg shadow-neutral-500 rounded-md absolute z-10b -bottom-14 right-12 py-3 dark:bg-gray-900   w-48  bg-white ${isOpen == true & _id == openId  ? '':'hidden'} `}>
+                                        <Clicks 
+                                        isOpen={isOpen} 
+                                        setIsOpen={setIsOpen} 
+                                        setOpenDelete={setOpenDelete} 
+                                        setOpenPublish={setOpenPublish}
+                                        employeeId={_id}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                                {eventManager  && !generalManager && <p>Event Manager</p>}
+                                {jobsManager && !generalManager &&<p>Jobs Manager</p>}
+                                {contentManager && !generalManager &&<p>Content Manager</p>}
+                                {biddingManager && !generalManager &&<p>Bidding Manager</p>}
+                                {responseManager &&!generalManager && <p>Response Manager</p>}
+                                {generalManager &&<p>General Manager</p>}
+                        </div>
+                    </div>
+                </div>
+            )
+        })}
+    </div>
+    {employee.length >= 3 && <ContentPages/>}
+        <dialog open={openDelete}>
+        <DeletePopUp 
+            openDelete={setOpenDelete} 
+            type={'Job'}
+            title={'Are you certain you want to remove this job?'}
+            description={"Once the job is deleted, it cannot be republished. If you later decided you want to make it available again, you'll need to recreate the job content from scratch. As an alternative, you can update your calendar's availability to reflect any changes."}
+            deleteHandler={deleteHandler}
+            />
+        </dialog>
+        <dialog open={openPublish}>
+        <UnPuplishPopUp 
+            setOpenPublish={setOpenPublish}
+            type={'Job'}
+            title={'Are you certain you want to unpublish this job?'}
+            description={"Do you want to view out this content for the next period? Then all you have to do is change the availability."}
+            unPublishHandler={unPublishHandler}
+            />
+        </dialog>
+    </>
   )
 }
 
