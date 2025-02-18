bd3c294ed516e63fb531972b1fdb768e375de7ca
Muzayen Adem <muzynadem@gmail.com>
2025-02-18 14:22:00 -0800

    chacges on mobile design

--- frontend/src/Controller/CommonLinks/ServerLink.js
+++ frontend/src/Controller/CommonLinks/ServerLink.js
@@ -1,4 +1,6 @@
-export const serverLink =  'https://dodola-official-website.onrender.com' 
+export const serverLink = 'https://dodola-official-website.onrender.com' 
+
+
 
 
 export const rentalHouseLink = 'https://gofind-om6x.onrender.com'
