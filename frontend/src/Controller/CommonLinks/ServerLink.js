22112d4bad274e9154ce7d8a9ef37fad9990890b
Muzayen Adem <muzynadem@gmail.com>
2025-12-16 21:59:39 +0300

    after long time

--- frontend/src/Controller/CommonLinks/ServerLink.js
+++ frontend/src/Controller/CommonLinks/ServerLink.js
@@ -1,9 +1,7 @@
 export const serverLink = 'https://dodola-official-website.onrender.com'
-//'http://localhost:3600'
 //'https://dodola-official-website.onrender.com'
 //'http://localhost:3600'
- //'https://dodola-official-website.onrender.com' 
-
 
  
+
 export const rentalHouseLink = 'https://gofind-om6x.onrender.com'
